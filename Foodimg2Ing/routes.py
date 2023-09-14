18d5d00af687b081d51f7d2071cc5cbedf16fa22
Logesh Kumar <logeshkumar.dev@gmail.com>
2023-09-14 23:57:45 +0530

    logesh-works

---
+++ Foodimg2Ing/routes.py
@@ -0,0 +1,29 @@
+from flask import render_template ,url_for,flash,redirect,request
+from Foodimg2Ing import app
+from Foodimg2Ing.output import output
+import os
+
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/about',methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/',methods=['POST','GET'])
+def predict():
+    imagefile=request.files['imagefile']
+    image_path=os.path.join(app.root_path,'static\\demo_imgs\\',imagefile.filename)
+    imagefile.save(image_path)
+    img="/images/demo_imgs/"+imagefile.filename
+    title,ingredients,recipe = output(image_path)
+    return render_template('predict.html',title=title,ingredients=ingredients,recipe=recipe,img=img)
+
+@app.route('/<samplefoodname>')
+def predictsample(samplefoodname):
+    imagefile=os.path.join(app.root_path,'static\\images',str(samplefoodname)+".jpg")
+    img="/images/"+str(samplefoodname)+".jpg"
+    title,ingredients,recipe = output(imagefile)
+    return render_template('predict.html',title=title,ingredients=ingredients,recipe=recipe,img=img)
---
+++ README.md
@@ -0,0 +1,18 @@
+### Prerequisite :
+Download these files and replace it with the files in this folder "Foodimg2Ing/data/"
+
+1. Model (Modelbest.ckpt) : [Download Modelbest.ckpt](https://dl.fbaipublicfiles.com/inversecooking/modelbest.ckpt)
+
+2. Ingredients (ingr_vocab.pkl) : [Download ingr_vocab.pkl](https://dl.fbaipublicfiles.com/inversecooking/ingr_vocab.pkl)
+
+3. Instruction (instr_vocab.pkl) : [Download instr_vocab.pkl](https://dl.fbaipublicfiles.com/inversecooking/instr_vocab.pkl)
+
+### To run the Code :
+
+1. Open Terminal (cmd)
+2. Install all the required libraries using "pip install -r requirements.txt"
+3. run "python run.py"
+
+You will be getting a localhost link and open that link in your browser 
+
+
